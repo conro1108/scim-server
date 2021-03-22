c9b89773502b6e0634177e221c6aa39522d19670
Connor Rowe <connorrowe@Connors-MacBook-Air.local>
2021-03-22 01:27:43 -0700

    spring boot init

---
+++ src/main/kotlin/com/example/scimapp/ScimappApplication.kt
@@ -0,0 +1,11 @@
+package com.example.scimapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScimappApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScimappApplication>(*args)
+}

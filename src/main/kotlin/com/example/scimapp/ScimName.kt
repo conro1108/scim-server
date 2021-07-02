2916af51335b291e454814ce82e1ab30512ef924
Connor Rowe <conro1108@gmail.com>
2021-07-01 21:42:21 -0700

    but make it kotlin

--- src/main/kotlin/com/example/scimapp/ScimName.kt
+++ src/main/kotlin/com/example/scimapp/ScimName.kt
@@ -1,23 +1,22 @@
 package com.example.scimapp
 
-class ScimName private constructor(builder: Builder){
+class ScimName private constructor(
+    val first: String?,
+    val middle: String?,
+    val last: String?,
+    val full: String?
+){
+    data class Builder (
+        var first: String? = null,
+        var middle: String? = null,
+        var last: String? = null,
+        var full: String? = null) {
 
-    val first: String? = builder.first
-    val middle: String? = builder.middle
-    val last: String? = builder.last
-    val full: String? = builder.full
+        fun first(first: String) = apply { this.first = first }
+        fun middle(middle: String) = apply { this.middle = middle }
+        fun last(last: String) = apply { this.last = middle }
+        fun full(full: String) = apply { this.full = full }
 
-    class Builder {
-        var first: String? = null
-        var middle: String? = null
-        var last: String? = null
-        var full: String? = null
-
-        fun first(first: String) { this.first = first }
-        fun middle(middle: String) { this.middle = middle }
-        fun last(last: String) { this.last = middle }
-        fun full(full: String) { this.full = full }
-
-        fun build() = ScimName(this)
+        fun build() = ScimName(first, middle, last, full)
     }
 }
